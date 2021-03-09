ee2cabacf53a9c21b203eb5fa5cd2cfdd26166b4
khabbs <44439534+khabbs@users.noreply.github.com>
2021-03-09 20:10:00 +0200

    Delete templates.py

--- Week_5/codes/templates.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, render_template
-app = Flask(__name__)
-
-@app.route("/")
-def templated():
-	return render_template("base.html", greeting="Hello Templates!")
-
-app.run(debug=True)
